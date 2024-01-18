d37c3ea6c35754f3e11273c3c06dfc6643a42f3a
Delhia0 <delhia.gbelidji@teamoty.io>
2024-01-18 20:09:08 +0100

    WIP

--- src/app/authentification/signup/page.tsx
+++ src/app/authentification/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Button, Stack, TextField } from "@mui/material";
+import { Button, Stack, TextField, Typography } from "@mui/material";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -13,43 +13,68 @@ type Type_data = {
 
 //Validation schema
 const Schema_SignUp = yup.object().shape({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
+  firstName: yup.string().required("Required"),
+  lastName: yup.string().required("Required"),
 });
 
 const SignUpForm = () => {
-  const { handleSubmit, control } = useForm<Type_data>({
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Type_data>({
     defaultValues: {
       firstName: "",
       lastName: "",
     },
     resolver: yupResolver(Schema_SignUp),
   });
-  const onSubmit: SubmitHandler<Type_data> = (data) => console.log(data);
+
+  const onSubmit: SubmitHandler<Type_data> = (data) => {
+    try {
+      console.log(data);
+    } catch {
+      console.error("error");
+    }
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack direction={"row"} spacing={2}>
-        <Controller
-          control={control}
-          name="firstName"
-          rules={{ required: true }}
-          render={({ field }) => (
-            <TextField {...field} label="First name" variant="outlined" />
-          )}
-        />
-        <Controller
-          control={control}
-          name="lastName"
-          rules={{ required: true }}
-          render={({ field }) => (
-            <TextField {...field} label="Last name" variant="outlined" />
-          )}
-        />
-      </Stack>
+    <Stack>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack direction={"row"} spacing={2}>
+          <Controller
+            control={control}
+            name="firstName"
+            rules={{ required: true }}
+            render={({ field, formState }) => (
+              <TextField
+                {...field}
+                label="First name"
+                variant="outlined"
+                error={!!formState.errors?.firstName}
+                helperText={errors.firstName?.message}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="lastName"
+            rules={{ required: true }}
+            render={({ field, formState }) => (
+              <TextField
+                {...field}
+                label="Last name"
+                variant="outlined"
+                error={!!formState.errors?.lastName}
+                helperText={errors.lastName?.message}
+              />
+            )}
+          />
+        </Stack>
 
-      <Button type="submit">Sign up</Button>
-    </form>
+        <Button type="submit">Sign up</Button>
+      </form>
+    </Stack>
   );
 };
 
