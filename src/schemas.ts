b10e0472116d0098f8ca33e99c4e19494589cdb7
Delhia0 <delhia.gbelidji@teamoty.io>
2024-01-27 16:23:49 +0100

    fix schema.ts

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,14 +1,14 @@
 import * as Yup from 'yup'
 
 export const Schema_SignUp = Yup.object().shape({
-    firstName: Yup.string().required("First name is required"),
-    lastName: Yup.string().required("Last name is required"),
-    phoneNumber: Yup.string().required("Phone number is required"),
+    firstname: Yup.string().required("First name is required"),
+    lastname: Yup.string().required("Last name is required"),
+    phone_number: Yup.string().required("Phone number is required"),
     email: Yup.string().email("Invalid email").required("Email is required"),
     password: Yup.string()
       .required("Password is required")
       .min(8, "Password must be at least 8 characters"),
-    confirmPassword: Yup.string()
+    confirm_password: Yup.string()
       .oneOf([Yup.ref("password")], "Passwords must match")
       .required("Confirm password is required"),
   });
