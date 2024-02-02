ca70526975390c2e9668948c55968ba45e595d10
Karim Fraboulet <karim.fraboulet@gmail.com>
2024-02-02 15:50:59 +0100

    Implementation Sign Up title

--- src/app/authentification/signup/page.tsx
+++ src/app/authentification/signup/page.tsx
@@ -1,16 +1,28 @@
 import { Box } from "@mui/material";
 import SignUpForm from "@/components/auth/SignUpForm";
+import Stack from '@mui/material/Stack';
+import '@fontsource/roboto/500.css';
+
 
 const SignUp = () => {
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="90vh"
-    >
+  <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    minHeight="90vh"
+  >
+    <Stack fontFamily="roboto" fontSize={15} direction="column" spacing={4} alignItems="center" sx={{fontWeight: 700}}>
+        <div>
+          <h1>
+            Join our adventure.<br/>
+            Sign up now, <br/>
+            and embark on your journey !
+          </h1>
+        </div>
       <SignUpForm />
-    </Box>
+    </Stack>
+  </Box>
   );
 };
 
