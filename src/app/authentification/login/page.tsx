7cdfa88b0ffbdfddf653972bc2973f63c3172b6d
Karim Fraboulet <karim.fraboulet@gmail.com>
2024-02-03 23:50:11 +0100

    Adding title of the form according to the figma model

--- src/app/authentification/login/page.tsx
+++ src/app/authentification/login/page.tsx
@@ -1,5 +1,9 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import LoginForm from "@/components/auth/LoginForm";
+import Stack from '@mui/material/Stack';
+import '@fontsource/roboto/500.css';
+
+
 
 const Login = () => {
   return (
@@ -9,7 +13,20 @@ const Login = () => {
       alignItems="center"
       minHeight="90vh"
     >
-      <LoginForm />
+          <Stack 
+      direction="column" 
+      spacing={4} 
+      alignItems="center" 
+    >
+          <Typography
+            fontSize={33}
+            sx={{fontWeight: 700}} 
+          >
+            Good to See You Again !<br />
+            Let’s Continue the Journey.
+          </Typography>
+        <LoginForm />
+      </Stack>
     </Box>
   );
 };
