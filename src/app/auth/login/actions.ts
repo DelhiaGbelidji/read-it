a25b3b275d3f2db71ce205a6f2882b8da344a226
Delhia0 <delhia.gbelidji@teamoty.io>
2024-02-12 18:37:30 +0100

    handle cookies

--- src/app/auth/login/actions.ts
+++ src/app/auth/login/actions.ts
@@ -1,12 +1,10 @@
 'use server'
-import { createSupabaseServerClient } from "@/utils/supabase/supabase";
+import { createSupabaseServerComponentClient } from "@/utils/supabase/supabase";
 import { Type_Login_FormData } from "@/utils/types";
-import { cookies } from "next/headers";
 
 
 export async function signInWithEmailAndPassword(data: Type_Login_FormData) {
-  const cookieStore = cookies()
-  const supabase = createSupabaseServerClient(cookieStore)
+	const supabase = createSupabaseServerComponentClient()
   const result = await supabase.auth.signInWithPassword(data);
 	return JSON.stringify(result);
 }
--- src/app/auth/signup/actions.ts
+++ src/app/auth/signup/actions.ts
@@ -1,11 +1,9 @@
 'use server'
-import { createSupabaseServerClient } from "@/utils/supabase/supabase";
+import { createSupabaseServerComponentClient } from "@/utils/supabase/supabase";
 import { Type_SignUp_FormData } from "@/utils/types";
-import { cookies } from "next/headers";
 
 export async function registerUser(data: Type_SignUp_FormData) {
-	const cookieStore = cookies()
-	const supabase = createSupabaseServerClient(cookieStore)
+	const supabase = createSupabaseServerComponentClient()
 	const result = await supabase.auth.signUp({
 		email: data.email,
 		password: data.password,
